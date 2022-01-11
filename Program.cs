56253db846a9b769073c5e00dd11bada31eeac01
Atish Ojha <ojhaatish11@gmail.com>
2022-01-11 13:37:22 +0545

    add and get percentage

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace add_and_get_percentage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] a = new double[5];
+            double sum = 0;
+            Console.WriteLine("Enter marks for 5 Subjects: (Adv. programming," +
+                "Research, BI, App. development, Web Development)");
+            for(int i = 0; i<5; i++)
+            {
+                double tempMarks = Convert.ToDouble(Console.ReadLine());
+                a[i] = tempMarks;
+                sum += tempMarks;
+            }
+            Console.WriteLine("The sum of all the marks is: {0}", sum);
+            Console.WriteLine("The percentage is: {0}%", sum / 500 * 100);
+            Console.ReadKey();
+        }
+    }
+}
